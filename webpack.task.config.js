50b447f87d356cc83ead61d7ef025ad8b43a800c
sant <santgutz2000@live.com>
2025-06-10 14:48:51 +0400

    Upgraded to react 18 and fixed issues

--- webpack.task.config.js
+++ webpack.task.config.js
@@ -4,14 +4,18 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
   entry: './src/task/task.ts',
   mode: 'production',
-  target: 'node',
+  target: 'node18.1',
   output: {
     path: path.resolve(__dirname, 'dist/task'),
     filename: 'task.js',
-    libraryTarget: 'commonjs2'
+    libraryTarget: 'commonjs2',
+    clean: false  // CRITICAL: Don't clean to avoid conflicts with tab build
   },
   resolve: {
     extensions: ['.ts', '.js']
+  },
+  externals: {
+    'azure-pipelines-task-lib': 'azure-pipelines-task-lib'
   },
   module: {
     rules: [
@@ -20,7 +24,11 @@ module.exports = {
         use: {
           loader: 'ts-loader',
           options: {
-            transpileOnly: true
+            transpileOnly: true,
+            compilerOptions: {
+              target: "es2018",
+              module: "commonjs"
+            }
           }
         },
         exclude: /node_modules/
@@ -28,11 +36,16 @@ module.exports = {
     ]
   },
   plugins: [
-    new CopyWebpackPlugin([
-      { from: 'src/task/task.json', to: 'task.json' }
-    ])
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'src/task/task.json', to: 'task.json' }
+      ]
+    })
   ],
   stats: {
     warnings: false
+  },
+  optimization: {
+    minimize: false
   }
 };
